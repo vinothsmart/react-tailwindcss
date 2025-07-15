3104da3e79ff8f74841dd84cd7b7fb8cef858c12
vinothsmart <s.vinothkanna90@gmail.com>
2025-07-15 09:26:45 +0530

    Add random descriptions to Header component for dynamic content display

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+const reactDesctiptions = [
+  "React is a JavaScript library for building user interfaces.",
+  "React allows developers to create large web applications that can change data, without reloading the page.",
+  "React is maintained by Facebook and a community of individual developers and companies.",
+];
+
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * max);
+};
+
 const Header = () => {
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {reactDesctiptions[getRandomInt(2)]} React concepts you will need for
+        almost any app you are going to build!
       </p>
     </header>
   );
