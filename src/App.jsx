5b6cc0ce2f8e9f2a67cc6bb2ae4ab3eb2615a585
vinothsmart <s.vinothkanna90@gmail.com>
2025-07-22 18:50:23 +0530

    tabs dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import TabButton from "./components/TabButton";
 import { useState } from "react";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("components");
+  const [selectedTopic, setSelectedTopic] = useState("");
   const handleTabSelection = (selectedTab) => () => {
     setSelectedTopic(selectedTab);
   };
@@ -36,13 +36,17 @@ function App() {
             <TabButton onSelect={handleTabSelection("props")}>Props</TabButton>
             <TabButton onSelect={handleTabSelection("state")}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
+          {!selectedTopic ? (
+            <p>Please select a topic.</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
         </section>
       </main>
     </div>
