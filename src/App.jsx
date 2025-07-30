61812e33653f5299959a9800ca6073b396e2730e
vinothsmart <s.vinothkanna90@gmail.com>
2025-07-30 15:43:05 +0530

    restore

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,69 @@
-import React from "react";
+import { EXAMPLES, REACT_CORE_CONCEPTS } from "./data";
+import Header from "./components/Header/Header";
+import CoreConcept from "./components/CoreConcept/CoreConcept";
+import TabButton from "./components/TabButton";
+import { useState } from "react";
 
-import Todo from "./Todo";
-
-// don't remove the export keyword here!
-export const DUMMY_TODOS = ["Learn React", "Practice React", "Profit!"];
-
-// don't change the Component name "App"
-export default function App() {
+function App() {
+  const [selectedTopic, setSelectedTopic] = useState("");
+  const handleTabSelection = (selectedTab) => () => {
+    setSelectedTopic(selectedTab);
+  };
   return (
-    <ul>
-      {DUMMY_TODOS.map((todo, index) => (
-        <Todo key={index} text={todo} />
-      ))}
-    </ul>
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts of React</h2>
+          <ul>
+            {REACT_CORE_CONCEPTS.map((concept, index) => (
+              <CoreConcept key={index} {...concept} />
+            ))}
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton
+              isActive={selectedTopic === "components"}
+              onSelect={handleTabSelection("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isActive={selectedTopic === "jsx"}
+              onSelect={handleTabSelection("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isActive={selectedTopic === "props"}
+              onSelect={handleTabSelection("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isActive={selectedTopic === "state"}
+              onSelect={handleTabSelection("state")}
+            >
+              State
+            </TabButton>
+          </menu>
+          {!selectedTopic ? (
+            <p>Please select a topic.</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
+        </section>
+      </main>
+    </div>
   );
 }
+
+export default App;
--- src/Todo.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Todo({ text = "Default Todo" }) {
-  return <li>{text}</li>;
-}
